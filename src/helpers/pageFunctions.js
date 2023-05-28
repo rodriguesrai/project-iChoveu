5e8d9c1ef785c4f2c3b88ccff40cf81419fa8f03
rodriguesrai <rairodrigues_sf@hotmail.com>
2023-05-28 20:21:12 -0300

    requisito 3 completo

--- src/helpers/pageFunctions.js
+++ src/helpers/pageFunctions.js
@@ -1,4 +1,4 @@
-import { searchCities } from './weatherAPI';
+import { getWeatherByCity, searchCities } from './weatherAPI';
 
 /**
  * Cria um elemento HTML com as informações passadas
@@ -116,6 +116,10 @@ export function handleSearch(event) {
 
   const searchInput = document.getElementById('search-input');
   const searchValue = searchInput.value;
-  searchCities(searchValue);
-  // seu código aqui
+  searchCities(searchValue)
+    .then((cidades) =>{
+      const urlCidades = cidades.map((cidade) => cidade.url);
+      const promiseCidades = urlCidades.map((cidade) => getWeatherByCity(cidade));
+      Promise.all(promiseCidades);
+    });
 }
--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -5,10 +5,10 @@ export const searchCities = (term) => {
   return fetch(`http://api.weatherapi.com/v1/search.json?key=${TOKEN}&q=${term}&aqi=no`)
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
       if (data.length === 0) {
         window.alert('Nenhuma cidade encontrada');
       }
+      return data;
     });
 };
 
@@ -16,6 +16,7 @@ export const getWeatherByCity = (cityURL) => {
   return fetch(`http://api.weatherapi.com/v1/current.json?lang=pt&key=${TOKEN}&q=${cityURL}`)
     .then((response) => response.json())
     .then((data) => {
+      console.log(data);
       const { temp_c: temp, condition: { text: condition, icon } } = data.current;
       return { temp, condition, icon };
     });
